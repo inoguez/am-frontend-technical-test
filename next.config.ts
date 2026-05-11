2ae12332cd84e69efea8900acb6c62482cc1b51e
Ian Noguez Gomez <sk8ian100@gmail.com>
2026-05-10 18:22:01 -0600

    chore(next): allow rickandmortyapi avatars in next/image

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      new URL('https://rickandmortyapi.com/api/character/avatar/**'),
+    ],
+  },
 };
 
 export default nextConfig;
