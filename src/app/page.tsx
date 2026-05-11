c46d61e7341c5e51c9d62c7d9de658eff5b25b7e
Ian Noguez Gomez <sk8ian100@gmail.com>
2026-05-10 18:26:02 -0600

    feat(app): wire home page with CharactersHome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import Image from 'next/image';
+import { Suspense } from 'react';
+
+import { CharactersHome } from '@/features/characters';
+
+import styles from './page.module.css';
+
+const Home = () => {
   return (
-    <div>
-      <main>{/** empty */}</main>
+    <div className={styles.page}>
+      <Image
+        height={95}
+        width={332}
+        src={'/rick-morty-logo.png'}
+        alt='rick and morty logo'
+      />
+      <main className={styles.main}>
+        {/* Requerido por `useSearchParams` durante el prerender. */}
+        <Suspense fallback={null}>
+          <CharactersHome />
+        </Suspense>
+      </main>
     </div>
   );
-}
+};
+
+export default Home;
